976feee6d9b870767c94f3facc9342930139e440
sabalero014 <sabalero014@users.noreply.github.com>
2020-10-19 18:41:46 -0300

    no se

--- aritmetic_aranger.py
+++ aritmetic_aranger.py
@@ -1,24 +1,24 @@
-def arithmetic_arranger(problems,display):
-    #solo se ejecuta si display is True
+def arithmetic_arranger(problems):
+    """#solo se ejecuta si display is True
     if display is not True:
         #print('saliendo')
-        exit()
-    #Variables a usar
-    todos = [] #todos los listados
-    resultados= []
-    primero=[]
+        exit()"""
+    # Variables a usar
+    todos = []  # todos los listados
+    resultados = []
+    primero = []
     segundo = []
     operador = []
     longitud = []
     relleno = " "
-    #check1 :5 problemas
+    # check1 :5 problemas
     if len(problems) > 5:
-        #máximo de 5 problemas
-        print("Error: Too many problems.")
-        exit()
+        # máximo de 5 problemas
+        arranged_problems = print("Error: Too many problems.")
+        return arranged_problems
 
     for k in problems:
-        #separar por espacio y agrego a una lista con valores
+        # separar por espacio y agrego a una lista con valores
         oper = k.split(' ')
         primero.append(oper[0])
         segundo.append(oper[2])
@@ -27,52 +27,64 @@ def arithmetic_arranger(problems,display):
     """print(f'Primeros: {primero}')
     print(f'segundos: {segundo}')
     print(f'Operadores {operador}')"""
-    def entero (listado):
-        #check3: enteros
+
+    def entero(listado):
+        # check3: enteros
         for k in listado:
             try:
                 int(k)
+                # arranged_problems ='ok'
             except:
-                print('Error: Numbers must only contain digits.')
+                arranged_problems = print('Error: Numbers must only contain digits.')
                 exit()
-            #check4: hasta 4 digitos
-            if len(k)>4:
-                print('Error: Numbers cannot be more than four digits.')
+                return arranged_problems
+            # check4: hasta 4 digitos
+            if len(k) > 4:
+                arranged_problems = print('Error: Numbers cannot be more than four digits.')
                 exit()
+                return arranged_problems
+
     entero(primero)
     entero(segundo)
-    def conjunto (listado):
+
+    def conjunto(listado):
         return todos.append(listado)
-    #obtengo todos los primeros términos de cada operación
+
+    # obtengo todos los primeros términos de cada operación
     conjunto(primero)
     conjunto(operador)
     conjunto(segundo)
-    #print(todos)
-    toditos = list(zip(primero,operador, segundo))
-    #print(toditos)
+    # print(todos)
+    toditos = list(zip(primero, operador, segundo))
+    # print(toditos)
     for i in operador:
-        #check2: operadores + o -
-        if i == "+": pass
-        elif i == "-": pass
+        # check2: operadores + o -
+        if i == "+":
+            pass
+        elif i == "-":
+            pass
         else:
-            print("Error: Operator must be '+' or '-'.")
-            exit()
+            arranged_problems = print("Error: Operator must be '+' or '-'.")
+            return arranged_problems
     for i in toditos:
-        #check2: operadores + o -
+        # vuelvo a hacer check de enteros
+        #entero(i[0])
+        #entero(i[2])
+        # check2: operadores + o -
         if i[1] == '+':
-            suma=int(i[0])+int(i[2])
+            suma = int(i[0]) + int(i[2])
             resultados.append(suma)
-            #print(f'Sumando: {suma}')
+            # print(f'Sumando: {suma}')
         elif i[1] == '-':
-            resta=int(i[0])-int(i[2])
+            resta = int(i[0]) - int(i[2])
             resultados.append(resta)
-            #print(f'Restando: {resta}')
+            # print(f'Restando: {resta}')
         else:
-            #print("Error: Operator must be '+' or '-'.")
-            exit()
+            arranged_problems = print("Error: Operator must be '+' or '-'.")
+            return arranged_problems
         longitud.append(max(len(i[0]), len(i[2])))
-    #print(resultados)
-    toditos = list(zip(primero,operador, segundo,resultados,longitud))
+    # print(resultados)
+    toditos = list(zip(primero, operador, segundo, resultados, longitud))
     conjunto(resultados)
     conjunto(longitud)
 
@@ -80,16 +92,16 @@ def arithmetic_arranger(problems,display):
     print(f'Toditos: muestra cada operación con su resultado \n{toditos}')
     print(f"Todos: muestra cada operador por separado. \n{todos}") #--- > esta es mejor"""
 
-    reng1="" #primer numero
-    reng2=""#simbolo mas segundo numero
-    divisor=""#lineas horizontales
-    reng3=""#resultados
+    reng1 = ""  # primer numero
+    reng2 = ""  # simbolo mas segundo numero
+    divisor = ""  # lineas horizontales
+    reng3 = ""  # resultados
     for i in toditos:
-        #componentes: (primero,operador, segundo,resultados,longitud)
-        reng1  += "  "+ i[0].rjust(i[4],relleno) + "    "
-        reng2  += i[1]+" "+ i[2].rjust(i[4],relleno) + "    "
-        divisor+= "-"* len("  "+ i[0].rjust(i[4],relleno))+"    "
-        reng3  += str(i[3]).rjust(len("-"* len("  "+ i[0].rjust(i[4],relleno))),relleno)+"    "
+        # componentes: (primero,operador, segundo,resultados,longitud)
+        reng1 += "  " + i[0].rjust(i[4], relleno) + "    "
+        reng2 += i[1] + " " + i[2].rjust(i[4], relleno) + "    "
+        divisor += "-" * len("  " + i[0].rjust(i[4], relleno)) + "    "
+        reng3 += str(i[3]).rjust(len("-" * len("  " + i[0].rjust(i[4], relleno))), relleno) + "    "
 
     """#para chequear
     print(reng1)
@@ -99,4 +111,7 @@ def arithmetic_arranger(problems,display):
     arranged_problems = print(f'{reng1}\n{reng2}\n{divisor}\n{reng3}')
     return arranged_problems
 
-arithmetic_arranger(["32 + 698", "3801 + 2", "9999 + 8878", "123 + 49"],True)
+
+arithmetic_arranger(["98 + 3g5", "3801 - 2", "45 + 43", "123 + 49"])
+# print("   ")
+# print("  11      3801      1      123         1\n+  4    - 2999    + 2    +  49    - 9380\n----    ------    ---    -----    ------")
