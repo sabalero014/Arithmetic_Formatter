624a91d9f52b67ed2a31bdefe99296b3a211214f
sabalero014 <sabalero014@users.noreply.github.com>
2020-10-19 16:11:14 -0300

    logrados los 4 check y tambien el de True

--- aritmetic_aranger.py
+++ aritmetic_aranger.py
@@ -1,14 +1,18 @@
 def arithmetic_arranger(problems,display):
+    #solo se ejecuta si display is True
+    if display is not True:
+        #print('saliendo')
+        exit()
     #Variables a usar
     todos = [] #todos los listados
     resultados= []
     primero=[]
     segundo = []
     operador = []
-    #checkear errores
+    #check1 :5 problemas
     if len(problems) > 5:
         #máximo de 5 problemas
-        print("Error: Too many problems")
+        print("Error: Too many problems.")
         exit()
 
     for k in problems:
@@ -18,29 +22,51 @@ def arithmetic_arranger(problems,display):
         segundo.append(oper[2])
         operador.append(oper[1])
 
-    print(f'Primeros: {primero}')
+    """print(f'Primeros: {primero}')
     print(f'segundos: {segundo}')
-    print(f'Operadores {operador}')
+    print(f'Operadores {operador}')"""
     def entero (listado):
-        #check enteros
+        #check3: enteros
         for k in listado:
             try:
                 int(k)
             except:
                 print('Error: Numbers must only contain digits.')
                 exit()
+            #check4: hasta 4 digitos
             if len(k)>4:
                 print('Error: Numbers cannot be more than four digits.')
                 exit()
     entero(primero)
     entero(segundo)
-    def conjunto (listado):
+    """def conjunto (listado):
         return todos.append(listado)
+    #obtengo todos los primeros términos de cada operación
     conjunto(primero)
     conjunto(operador)
     conjunto(segundo)
-    
+    print(todos)"""
+    toditos = list(zip(primero,operador, segundo))
+    #print(toditos)
+    for i in toditos:
+        #check2: operadores + o -
+        if i[1] != '+' or i[1] != '-':
+            print("Error: Operator must be '+' or '-'.")
+            exit()
+    for i in toditos:
+        #check2: operadores + o -
+        if i[1] == '+':
+            suma=int(i[0])+int(i[2])
+            resultados.append(suma)
+            print(f'Sumando: {suma}')
+        elif i[1] == '-':
+            resta=int(i[0])-int(i[2])
+            resultados.append(resta)
+            print(f'Restando: {resta}')
+        else:
+            #print("Error: Operator must be '+' or '-'.")
+            exit()
 
 #    return arranged_problems
 
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"],True)
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 * 49"],True)
