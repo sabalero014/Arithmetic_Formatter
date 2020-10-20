ba9a1c35a6abc50439838385c02f10ffc2f288eb
sabalero014 <sabalero014@users.noreply.github.com>
2020-10-19 22:09:39 -0300

    versión aprobada!!

--- aritmetic_aranger.py
+++ aritmetic_aranger.py
@@ -1,5 +1,4 @@
-def arithmetic_arranger(problems,est = True):
-    if est != True: exit()
+def arithmetic_arranger(problems,est = False):
     # Variables a usar
     todos = []  # todos los listados
     resultados = []
@@ -12,8 +11,7 @@ def arithmetic_arranger(problems,est = True):
     # check1 :5 problemas
     if len(problems) > 5:
         # máximo de 5 problemas
-        arranged_problems = print("Error: Too many problems.")
-        return arranged_problems
+        return "Error: Too many problems."
 
     for k in problems:
         # separar por espacio y agrego a una lista con valores
@@ -38,36 +36,32 @@ def arithmetic_arranger(problems,est = True):
         elif i == "-":
             pass
         else:
-            arranged_problems = print(check2)
+            #arranged_problems = print(check2)
             #print("Error: Operator must be '+' or '-'.")
-            return arranged_problems
+            return "Error: Operator must be '+' or '-'."
     # check4: hasta 4 digitos
     def largo (entrada):
         if len(entrada) > 4:
-            arranged_problems = print('Error: Numbers cannot be more than four digits.')
-            return arranged_problems
+            #arranged_problems = print('Error: Numbers cannot be more than four digits.')
+            return 'Error: Numbers cannot be more than four digits.'
     for i in toditos:
         # check4: hasta 4 digitos
         if len(i[0]) > 4:
-            arranged_problems = print('Error: Numbers cannot be more than four digits.')
+            #arranged_problems = print('Error: Numbers cannot be more than four digits.')
             #print('Error: Numbers cannot be more than four digits.')
-            return arranged_problems
+            return 'Error: Numbers cannot be more than four digits.'
         if len(i[2]) > 4:
-            arranged_problems = print('Error: Numbers cannot be more than four digits.')
+            #arranged_problems = print('Error: Numbers cannot be more than four digits.')
             #print('Error: Numbers cannot be more than four digits.')
-            return arranged_problems
+            return 'Error: Numbers cannot be more than four digits.'
         #check enteros a cada valores
         try:
             a = int(i[0])
             b = int(i[2])
-        except ValueError:
-            arranged_problems = print('Error: Numbers must only contain digits.')
-            #exit()
-            return arranged_problems
         except:
-            arranged_problems = print('otra cosa')
+            #arranged_problems = print('Error: Numbers must only contain digits.')
             #exit()
-            return arranged_problems
+            return 'Error: Numbers must only contain digits.'
         # check2: operadores + o - y hace la operatoria
         if i[1] == '+':
             suma = a+b
@@ -78,8 +72,8 @@ def arithmetic_arranger(problems,est = True):
             resultados.append(resta)
             #print(f'Restando: {resta}')
         else:
-            arranged_problems = print(check2)
-            return arranged_problems
+            #arranged_problems = print(check2)
+            return "Error: Operator must be '+' or '-'."
         longitud.append(max(len(i[0]), len(i[2])))
     # print(resultados)
     toditos = list(zip(primero, operador, segundo, resultados, longitud))
@@ -96,13 +90,13 @@ def arithmetic_arranger(problems,est = True):
         reng2 += i[1] + " " + i[2].rjust(i[4], relleno) + "    "
         divisor += "-" * len("  " + i[0].rjust(i[4], relleno)) + "    "
         reng3 += str(i[3]).rjust(len("-" * len("  " + i[0].rjust(i[4], relleno))), relleno) + "    "
+    if est == True:
+        #arranged_problems = print(f'{reng1.rstrip()}\n{reng2.rstrip()}\n{divisor.rstrip()}\n{reng3.rstrip()}')
+        return reng1.rstrip()+"\n"+reng2.rstrip()+"\n"+divisor.rstrip()+"\n"+reng3.rstrip()
+    else:
+        #arranged_problems = print(f'{reng1.rstrip()}\n{reng2.rstrip()}\n{divisor.rstrip()}')
+        return reng1.rstrip() + "\n" + reng2.rstrip() + "\n" + divisor.rstrip()
 
-    arranged_problems = print(f'{reng1.rstrip()}\n{reng2.rstrip()}\n{divisor.rstrip()}\n{reng3.rstrip()}')
-    return arranged_problems
-
-entrada =["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]
-arithmetic_arranger(entrada)
-print(len("    3      3801      45      123"))
-
-print("    3      3801      45      123\n+ 855    -    2    + 43    +  49\n-----    ------    ----    -----")
-print((len("    3      3801      45      123")))
+arithmetic_arranger(["32 - 698", "1 - 3801", "45 + 43", "123 + 49"], True)
+print("        ")
+print("   32         1      45      123\n- 698    - 3801    + 43    +  49\n-----    ------    ----    -----\n -666     -3800      88      172")
--- pruebas-sueltas.py
+++ pruebas-sueltas.py
@@ -1,5 +1,68 @@
-random_string = ' this is good   '
+def arithmetic_arranger(problems, op_par=False):
+    first = ""
+    second = ""
+    answer = ""
+    line = "-"
+    lines = ''
 
-# Leading whitepsace are removed
-print(random_string.rstrip())
-print(random_string)
+    # ERROR #
+    # TOO MANY PROBLEMS (LIMIT IS 5)
+    if len(problems) >= 6:
+        return 'Error: Too many problems.'
+
+    for i in problems:
+        operation = i.split()
+        first_number = operation[0]
+        operator_digit = operation[1]
+        second_number = operation[2]
+
+        # ERROR #
+        # NUMBER HAS MORE THAN 4 DIGITS
+        if len(first_number) > 4 or len(second_number) > 4:
+            return "Error: Numbers cannot be more than four digits."
+
+        # ERROR #
+        # NUMBER CONTAINS A LETTER CHARACTER
+        if not first_number.isnumeric() or not second_number.isnumeric():
+            return "Error: Numbers must only contain digits."
+
+        # ERROR #
+        # OPERATOR IS '/' OR '*'
+        if operator_digit == '+' or operator_digit == '-':
+            if operator_digit == '+':
+                result = str(int(first_number) + int(second_number))
+            else:
+                result = str(int(first_number) - int(second_number))
+
+            length = max(len(first_number), len(second_number)) + 2
+            top_line = first_number.rjust(length)
+            bottom_line = operator_digit + second_number.rjust(length - 1)
+            result_line = result.rjust(length)
+
+            if i != problems[-1]:
+                first += top_line + '    '
+                second += bottom_line + '    '
+                lines += line*length + '    '
+                answer += result_line + '    '
+
+            else:
+                first += top_line
+                second += bottom_line
+                lines += line * length
+                answer += result_line
+
+        else:
+            return "Error: Operator must be '+' or '-'."
+
+    first.rstrip()
+    second.rstrip()
+    lines.rstrip()
+
+    if op_par:
+        arranged_problems = first + "\n" + second + "\n" + lines + "\n" + answer
+
+    else:
+        arranged_problems = first + "\n" + second + "\n" + lines
+
+    return arranged_problems
+
